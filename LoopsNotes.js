4fadb2d40d2dcefd9792b1118872ffb1610734b5
nic0mp <nico.mp215@gmail.com>
2022-01-30 21:36:16 -0600

    Looping through arrays

--- LoopsNotes.js
+++ LoopsNotes.js
@@ -7,3 +7,13 @@ for (let nums=5; nums<=10; nums++) {
 for (let counter = 3; counter >= 0; counter--){
     console.log(counter);
   }
+
+//   Looping through Arrays
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log(vacationSpots[i]);
+}
+// OUTPUT: 
+// Bali
+// Paris
+// Tulum
