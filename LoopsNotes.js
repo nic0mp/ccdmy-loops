fda86c6d1ec186dd24bfeaa209dc64416a2b1286
nic0mp <nico.mp215@gmail.com>
2022-01-30 20:59:09 -0600

    reverse loop

--- LoopsNotes.js
+++ LoopsNotes.js
@@ -3,5 +3,7 @@ for (let nums=5; nums<=10; nums++) {
     console.log(nums)
   };
   
-  
-  
+// Reverse Loop/ Descending order  
+for (let counter = 3; counter >= 0; counter--){
+    console.log(counter);
+  }
