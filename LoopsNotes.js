0a60bb5bce5faa72e6d8a39cc7186f4129f2fcc8
nic0mp <nico.mp215@gmail.com>
2022-02-01 15:42:00 -0600

    break

--- LoopsNotes.js
+++ LoopsNotes.js
@@ -74,3 +74,20 @@ do {
   cupsAdded = cupsAdded + 1
   console.log(cupsAdded + ' cup was added')
 } while ( cupsAdded < cupsOfSugarNeeded);
+
+// break
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write your code below
+for (let i=0; i<rapperArray.length; i++){
+  console.log(rapperArray[i]);
+  if (rapperArray[i] === 'Notorious B.I.G.'){
+    break
+  }
+}
+console.log("And if you don't know, now you know.")
+// OUTPUT:
+// Lil' Kim
+// Jay-Z
+// Notorious B.I.G.
+// And if you don't know, now you know.
