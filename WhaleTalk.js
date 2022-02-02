5f5208e72fea98b5247b297f78ebd55b7a54dbe1
nic0mp <nico.mp215@gmail.com>
2022-02-02 14:46:50 -0600

    Whale talk with notes

--- WhaleTalk.js
+++ WhaleTalk.js
@@ -1,26 +1,30 @@
-let input = "great chin chunks"
-// need an array of vowels to be extracted from the input variable
+const input = "great chin chunks"
+// create an array of vowels to be extracted from the input variable. 
+// Set the array equal to a variable named vowels
 const vowels = ['a', 'e', 'i', 'o', 'u'];
-//Create a variable named resultArray and set it equal to an empty array
-const resultArr = [];
+// Create a variable named resultArray and set it equal to an empty array: []. 
+// This will serve as a place to store the vowels from the input string.
+let resultArr = [];
 // Create a loop to iterate through each letter of the input variable text
 for(let i=0; i<input.length; i++) {
   // console.log(i);
-// Create a nested for loop inside of the for loop you just wrote. 
-// Make the inner loop iterate through the vowels array each time the outer loop runs.  
-// To check each letter of input against all the vowels elements during each iteration.
+// Create a nested for loop inside of the for loop 
+// Make the inner loop iterate through the vowels
   for(let j=0; j<vowels.length; j++) {
-//  write a code block that compares the input letter to every letter in the vowels array
-    if(input[i]===vowels[j]) {
-      if(input[i] === 'e'){
-        resultArr.push('ee')
-      } else if(input[i] === 'u'){
-        resultArr.push('uu')
-      }
-      else {
-        resultArr.push(input[i])
-      }
+// write a code block that compares the input letter to every letter in the vowels array.
+    if(input[i] === vowels[j]) {
+// Use the .push() method to add matching letters to resultArr
+      resultArr.push(vowels[j]);
     }
+// Write an if statement that checks if each letter in the input string is equal to 'e'. 
+// If so, .push() input[i] to the resultArray
+// this statement belongs after the inner for loop block because you only want to 
+// perform this check once for every letter in the input
+  } if (input[i] === 'e'){
+    resultArr.push(input[i]);
+  } if (input[i] === 'u'){
+    resultArr.push(input[i]);
   }
 }
-console.log(resultArr.join('').toUpperCase())
+console.log(resultArr.join('').toUpperCase());
+
